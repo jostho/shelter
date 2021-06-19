b9f1d2420634d3ad88ead65c391e7481c58a6b58
Jose Thomas <jostho@gmail.com>
2021-06-19 13:19:11 +0530

    Adding shelter sources

---
+++ shelter.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python3
+
+# tested with python 3.9
+
+# url shortener server
+
+import ipaddress
+import pickle
+import random
+import string
+import time
+from pathlib import Path
+
+import click
+from flask import Flask, Response, jsonify, make_response, redirect, request
+
+DEFAULT_HOST = "0.0.0.0"
+DEFAULT_PORT = 5000
+
+DB_FILE = "/tmp/shelter.pickle"
+DEFAULT_KEY_LENGTH = 4
+
+CONTENT_TYPE_TEXT = "text/plain"
+CONTENT_TYPE_JSON = "application/json"
+
+# allow list for protected urls
+CIDR_ALLOW_LIST = []
+
+# requests per minute limit for throttled urls
+LIMIT_RPM = 15
+
+# number of nano seconds in a minute
+ONE_MINUTE_IN_NANOS = 60 * 1000 * 1000 * 1000
+
+# track ips for throttled urls
+rate_limit_ip_tracker = {}
+
+app = Flask(__name__)
+
+
+def _prepare_url_dict(input_json):
+    epoch = input_json["epoch"] if "epoch" in input_json else time.time_ns()
+    return dict(url=input_json["url"], epoch=epoch, hits=0)
+
+
+def _get_random_key(key_length=DEFAULT_KEY_LENGTH):
+    return "".join(random.choices(string.ascii_uppercase, k=key_length))
+
+
+def _init():
+    db = {"urls": {}}
+    _save(db)
+
+
+def _read():
+    db = {"urls": {}}
+    if Path(DB_FILE).is_file():
+        with open(DB_FILE, "rb") as infile:
+            db = pickle.load(infile)
+    return db
+
+
+def _save(db):
+    with open(DB_FILE, "wb") as outfile:
+        pickle.dump(db, outfile)
+
+
+def _add_url(input_json):
+    db = _read()
+    item = None
+    if "url" in input_json:
+        s_key = _get_random_key()
+        if s_key not in db["urls"]:
+            s_val = _prepare_url_dict(input_json)
+            item = {s_key: s_val}
+            db["urls"].update(item)
+            _save(db)
+        else:
+            item = dict(message="key is taken")
+    else:
+        item = dict(message="url not found")
+    return item
+
+
+def _get_urls():
+    db = _read()
+    return db["urls"]
+
+
+def _get_url_item_for_api(key):
+    db = _read()
+    item = None
+    if key in db["urls"]:
+        item = db["urls"][key]
+    else:
+        item = dict(message="url not found")
+    return item
+
+
+def _get_url_for_redirect(key):
+    db = _read()
+    url = None
+    if key in db["urls"]:
+        url = db["urls"][key]["url"]
+        db["urls"][key]["hits"] += 1
+        _save(db)
+    return url
+
+
+def _status():
+    db = _read()
+    now = time.time_ns()
+    # provide only IPs active in the last 1 minute
+    ip_tracker = {
+        key: val
+        for key, val in rate_limit_ip_tracker.items()
+        if now < val["epoch"] + ONE_MINUTE_IN_NANOS
+    }
+    return dict(total_url=len(db["urls"]), ip_tracker=ip_tracker)
+
+
+def _ip_local(remote_ip):
+    return remote_ip == "127.0.0.1"
+
+
+def _ip_allowed(remote_ip):
+    if _ip_local(remote_ip):
+        return True
+    for cidr in CIDR_ALLOW_LIST:
+        if ipaddress.ip_address(remote_ip) in ipaddress.ip_network(cidr):
+            return True
+    # if you reach here, bad luck
+    return False
+
+
+def _refill_ip():
+    return dict(epoch=time.time_ns(), available=LIMIT_RPM - 1)
+
+
+def _ip_throttled(remote_ip):
+    if _ip_local(remote_ip):
+        return False
+    if remote_ip in rate_limit_ip_tracker:
+        ip_details = rate_limit_ip_tracker[remote_ip]
+        now = time.time_ns()
+        # refill if more than a minute has passed
+        if now > ip_details["epoch"] + ONE_MINUTE_IN_NANOS:
+            rate_limit_ip_tracker[remote_ip] = _refill_ip()
+            return False
+        # decrement if you are inside the one minute
+        if ip_details["available"] > 0:
+            ip_details["available"] -= 1
+            rate_limit_ip_tracker[remote_ip] = ip_details
+            return False
+    else:
+        rate_limit_ip_tracker[remote_ip] = _refill_ip()
+        return False
+    # if you reach here, bad luck
+    return True
+
+
+@app.route("/")
+def index():
+    return "Welcome to shelter"
+
+
+@app.route("/healthcheck")
+def healthcheck():
+    return Response(response="Ok", status=200, content_type=CONTENT_TYPE_TEXT)
+
+
+@app.route("/sleep/<int:seconds>")
+def sleep(seconds):
+    max_seconds = 10
+    if _ip_allowed(request.remote_addr) and seconds < max_seconds:
+        time.sleep(seconds)
+        return Response(response="Woke", status=200, content_type=CONTENT_TYPE_TEXT)
+    else:
+        return Response(
+            response="Forbidden", status=403, content_type=CONTENT_TYPE_TEXT
+        )
+
+
+@app.route("/init", methods=["POST"])
+def init():
+    if _ip_allowed(request.remote_addr):
+        _init()
+        return Response(response="Ok", status=200, content_type=CONTENT_TYPE_TEXT)
+    else:
+        return Response(
+            response="Forbidden", status=403, content_type=CONTENT_TYPE_TEXT
+        )
+
+
+@app.route("/status")
+def status():
+    if _ip_allowed(request.remote_addr):
+        result = _status()
+        return jsonify(result)
+    else:
+        result = dict(message="Forbidden")
+        return make_response(jsonify(result), 403)
+
+
+@app.route("/api/<key>")
+def api_with_key(key):
+    if not _ip_throttled(request.remote_addr):
+        result = _get_url_item_for_api(key)
+        if "message" in result:
+            return make_response(jsonify(result), 400)
+        else:
+            return jsonify(result)
+    else:
+        result = dict(message="Too Many Requests")
+        return make_response(jsonify(result), 429)
+
+
+@app.route("/api", methods=["GET", "POST"])
+def api():
+    if not _ip_throttled(request.remote_addr):
+        if request.method == "POST":
+            if request.get_json():
+                result = _add_url(request.get_json())
+                if "message" in result:
+                    return make_response(jsonify(result), 400)
+            else:
+                result = dict(message="request is not json")
+                return make_response(jsonify(result), 400)
+        else:
+            result = _get_urls()
+        return jsonify(result)
+    else:
+        result = dict(message="Too Many Requests")
+        return make_response(jsonify(result), 429)
+
+
+@app.route("/s/<key>")
+def short(key):
+    url = _get_url_for_redirect(key)
+    if url:
+        return redirect(url)
+    else:
+        return Response(
+            response="Not Found", status=404, content_type=CONTENT_TYPE_TEXT
+        )
+
+
+@click.command()
+@click.option(
+    "--host",
+    "-h",
+    default=DEFAULT_HOST,
+    show_default=True,
+    help="host",
+)
+@click.option(
+    "--port",
+    "-p",
+    default=DEFAULT_PORT,
+    show_default=True,
+    help="port",
+)
+def cli(host, port):
+    """shelter server"""
+    app.run(host=host, port=port)
+
+
+if __name__ == "__main__":
+    cli(auto_envvar_prefix="SHELTER")
